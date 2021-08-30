f053223c28f1098fd9cbb184582197b3d039c678
LuSntgo <lu.sntgo@gmail.com>
2021-08-30 19:56:25 -0300

    Iniciando BestBank

---
+++ bestbank/bin/main.dart
@@ -0,0 +1,17 @@
+void main() {
+  string titular = "Gabriel";
+  int agencia = 123;
+  int conta = 145;
+  double saldo = 10.0;
+
+  string titular1 = "José";
+  int agencia1 = 123;
+  int conta1 = 146;
+  double saldo1 = 0.0;
+
+  string titular2 = "Gabriela";
+  int agencia2 = 123;
+  int conta2 = 147;
+  double saldo2 = 20.0
+}
+git s
