1f1b49b2fb81e34ade04fb87d2b77623ec8658f2
LuSntgo <lu.sntgo@gmail.com>
2021-08-31 15:01:26 -0300

    Adicionados metodos para movimentação da conta

--- bestbank/bin/main.dart
+++ bestbank/bin/main.dart
@@ -1,26 +1,66 @@
 void main() {
   ContaCorrente contadaAmanda = ContaCorrente();
-  contadaAmanda.titular = "Amanda";
-  contadaAmanda.agencia = 123;
-  contadaAmanda.conta = 1;
-
-  print("Titular: ${contadaAmanda.titular}");
-  print("Agência: ${contadaAmanda.agencia}");
-  print("Saldo: ${contadaAmanda.saldo}");
-
   ContaCorrente contadoTiago = ContaCorrente();
+  contadaAmanda.titular = "Amanda";
+  contadaAmanda.chequeEspecial = -110.0;
   contadoTiago.titular = "Tiago";
-  contadoTiago.agencia = 123;
-  contadoTiago.conta = 2;
 
-  print("Titular: ${contadoTiago.titular}");
-  print("Agência: ${contadoTiago.agencia}");
-  print("Saldo: ${contadoTiago.saldo}");
+/*   contadaAmanda.deposito(20.0);
+  print("Saldo da ${contadaAmanda.titular}: ${contadaAmanda.saldo}");
+  contadaAmanda.saque(80.0);
+  print("Saldo da ${contadaAmanda.titular}: ${contadaAmanda.saldo}");
+
+  contadoTiago.deposito(50.0);
+  print("Saldo da ${contadoTiago.titular}: ${contadoTiago.saldo}");
+  contadoTiago.saque(130.0);
+  print("Saldo da ${contadoTiago.titular}: ${contadoTiago.saldo}"); */
+
+  print("Saldo da ${contadaAmanda.titular}: ${contadaAmanda.saldo}");
+  print("Saldo da ${contadoTiago.titular}: ${contadoTiago.saldo}");
+  bool sucesso = contadaAmanda.transferencia(130.0, contadoTiago);
+  print(sucesso);
+  print("Saldo da ${contadaAmanda.titular}: ${contadaAmanda.saldo}");
+  print("Saldo da ${contadoTiago.titular}: ${contadoTiago.saldo}");
 }
 
 class ContaCorrente {
   String titular = '';
-  int agencia = 0;
+  int agencia = 145;
   int conta = 0;
-  double saldo = 30.0; //Promoção: Faça o cadastro e ganhe 30 de saldo
+  double saldo = 20.0; //Promoção: Faça o cadastro e ganhe 20 de saldo
+  double chequeEspecial = -100.0;
+
+  bool verificaSaldo(double valor) {
+    if (this.saldo - valor < chequeEspecial) {
+      print("Sem saldo suficiente.");
+      return false;
+    } else {
+      print("Sacando $valor reais.");
+      return true;
+    }
+  }
+
+  bool transferencia(double valorDeTransferencia, ContaCorrente contaDestino) {
+    if (!verificaSaldo(valorDeTransferencia)) {
+      return false;
+    } else {
+      this.saldo -= valorDeTransferencia;
+      contaDestino.deposito(valorDeTransferencia);
+      return true;
+    }
+  }
+
+  bool saque(double valorDoSaque) {
+    if (!verificaSaldo(valorDoSaque)) {
+      return false;
+    } else {
+      this.saldo -= valorDoSaque;
+      return true;
+    }
+  }
+
+  double deposito(double valorDoDeposito) {
+    this.saldo += valorDoDeposito;
+    return this.saldo;
+  }
 }
